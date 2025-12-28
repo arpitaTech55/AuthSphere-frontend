b26bc4647cd82a69bb70555ac79c33d59b02b876
arpitaTech55 <arpitaaggarwal9120@gmail.com>
2025-12-29 00:18:01 +0530

    Signup.jsx updated

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -27,7 +27,7 @@ function Signup() {
     }
 
     try {
-      const response = await fetch("https://auth-sphere-backend.vercel.app/signup", {
+      const response = await fetch("https://auth-sphere-backend.vercel.app/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(signupInfo),
